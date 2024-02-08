34281f6a53107dde78e890dc9c97e59e15761385
safaa tazin <144169864+safaatazin@users.noreply.github.com>
2024-02-08 14:59:23 -0700

    Create safaamanageacess.java

---
+++ safaamanageacess.java
@@ -0,0 +1,22 @@
+package edu.ucalgary.oop;
+
+public class ManageAccess {
+    private AccessLog[] completeLog = new AccessLog[10];
+
+    // Constructor given a log file of up to 10 entries, create
+    // an array of AccessLog objects.
+    public ManageAccess(String[] logFile) throws IllegalArgumentException {
+        for (int i = 0; i < logFile.length; i++) {
+            var logLine = new AccessLog(logFile[i]);
+            completeLog[i] = logLine;
+        }
+    }
+
+    // Getter: return a specific AccessLog object
+    public AccessLog getLogLine(int i) throws IllegalArgumentException {
+        if (i >= 0 && i < 10) {
+            return completeLog[i];
+        }
+        throw new IllegalArgumentException("Array only holds indices 0-9");
+    }
+}
